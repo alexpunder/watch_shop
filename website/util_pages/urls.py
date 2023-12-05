5de95b1ad2d4dbfae8079b2edbff49e046ff26e7
Александр Литвинов <alexpunder@yandex.ru>
2023-12-05 18:45:17 +0300

    Предварительная настройка приложений и шаблонов.

---
+++ website/util_pages/urls.py
@@ -0,0 +1,9 @@
+# from django.urls import path
+
+
+# urlpatterns = [
+#     path('about/', About.as_view(), name='about'),
+#     path('contacts/', Contacts.as_view(), name='contacts'),
+#     path('confidential/', Confidential.as_view(), name='confidential'),
+#     path('terms-and-conditions/', Terms.as_view(), name='terms'),
+# ]
