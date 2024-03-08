487fe06b64011b56168bc247d6047a917170998b
Александр Литвинов <alexpunder@yandex.ru>
2024-03-08 15:40:16 +0300

    Поправлена главная страница сайта.

--- website/website/settings.py
+++ website/website/settings.py
@@ -1,7 +1,11 @@
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
